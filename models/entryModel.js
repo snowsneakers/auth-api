5815d8a093a00fa1934b0892b7962a58ab308b8a
Patrick Snowden <patrickbsnowden@outlook.com>
2022-09-08 00:35:12 -0700

    added user id and username to each post

--- models/entryModel.js
+++ models/entryModel.js
@@ -7,7 +7,8 @@ const entrySchema = new mongoose.Schema(
           text: { type: String, required: true },
           likes: { type: Array, requried: true, default: [] },
           liked: { type: Boolean, required: true, default: false },
-          user_username: { type: String, requried: true },
+          user_id: { type: String, requried: true },
+          user_username: {type: String, required: true}
      },
      { timestamps: true }
 );
