e55874dbd04c67f76b405387ee7c767ef4e72f69
Patrick Snowden <patrickbsnowden@outlook.com>
2022-09-09 22:59:26 -0700

    added profile picture route and get user by postid

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -3,11 +3,16 @@ const {
      loginUser,
      signupUser,
      getProfile,
+     updateAvatar,
+     getUserByPostId
 } = require("../controllers/userControllers");
+const protect = require("../middleware/protect");
 
 router.post("/login", loginUser);
 router.post("/signup", signupUser);
 //maybe another route for other profiles
 router.get("/profile/:username", getProfile);
+router.get("/:postId", getUserByPostId)
+router.put("/updateAvatar", protect, updateAvatar)
 
 module.exports = router;
